9846bbbc575781064787b7b6ecb898728e1e2050
ted.wen <jhwen840220@gmail.com>
2022-02-28 23:31:05 +0800

    test cicd

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,7 +24,7 @@ export default function Home({ post }: HomeProps) {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!!</a>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <h1 className="text-3xl font-bold underline">{post?.title}</h1>
